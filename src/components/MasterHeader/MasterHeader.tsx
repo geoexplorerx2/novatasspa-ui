cf3de435d08d85170bac68d3230dc0a832c431c5
Laurent Kouassi <laurent.kouassi@hotelistan.com>
2023-03-22 22:45:04 +0300

    master header progress

--- src/components/MasterHeader/MasterHeader.tsx
+++ src/components/MasterHeader/MasterHeader.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const MasterHeader = () => {
   return (
-    <div className='h-[100px]'>
-        MasterHeader
+    <div className='sticky h-[123px] top-0'>
+        <div className="container py-5 relative flex justify-between items-center overflow-hidden">
+            <div className="flex justify-start">
+                Menu
+            </div>
+
+            <div className="flex justify-center">
+                Logo
+            </div>
+
+            <div className="flex justify-end">
+                Language
+            </div>
+        </div>
+        <div className='h-[1px] bg-[#666B45] opacity-10'></div>
     </div>
   )
 };
