f4484ddb28c5ed01b644f84c098e0af865198d47
MohammedJR <mohammed.j@hotelistan.com>
2023-03-24 17:09:47 +0300

    fix: the OurServices section positioning

--- src/views/Homepage/OurServices.tsx
+++ src/views/Homepage/OurServices.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as CaldleAndFlower } from '../../assets/icons/candleFlow
 
 const OurServices: FC = () => {
     return (
-        <div className='container'>
-            <h2 className='text-white text-[64px] font-gotu leading-[74px] tracking-tighter'>Our <br /> Services</h2>
+        <div className='container -mt-[430px]'>
+            <h2 className='text-white text-[64px] font-gotu leading-[74px] tracking-tighter mb-12'>Our <br /> Services</h2>
             <div className='grid grid-cols-4 gap-[30px]'>
                 {ServicesData.map(service => <ServiceCard service={service} />)}
             </div>
@@ -28,11 +28,11 @@ interface ServiceCardProps {
 const ServiceCard: FC<ServiceCardProps> = (props) => {
     const { service: { description, Icon, id, price, title } } = props
     return (
-        <div className='border border-[#EEEEEE] py-[42px] px-[35px] space-y-[30px]' key={id}>
+        <div className='border border-[#EEEEEE] py-[42px] px-[35px] space-y-[30px] bg-white' key={id}>
             <div className='flex justify-between'>
                 <Icon />
                 <div>
-                    <span className='font-medium text-2xl'>
+                    <span className='font-medium text-2xl mr-1'>
                         {price}
                     </span>
                     <span className='text-xs inline-block -translate-y-1/2 font-medium'>$</span>
