b542f94c25d68e494749ee3e2711cd758b5980a9
MohammedJR <mohammed.j@hotelistan.com>
2023-03-24 17:02:47 +0300

    feat: the our services section

--- src/views/Homepage/OurServices.tsx
+++ src/views/Homepage/OurServices.tsx
@@ -1,9 +1,104 @@
 import React, { FC } from 'react'
+import { ReactComponent as CaldleAndFlower } from '../../assets/icons/candleFlower.svg'
+
 
 const OurServices: FC = () => {
-  return (
-    <div>OurServices</div>
-  )
+    return (
+        <div className='container'>
+            <h2 className='text-white text-[64px] font-gotu leading-[74px] tracking-tighter'>Our <br /> Services</h2>
+            <div className='grid grid-cols-4 gap-[30px]'>
+                {ServicesData.map(service => <ServiceCard service={service} />)}
+            </div>
+        </div>
+    )
 }
+
+interface ServiceType {
+    title: string,
+    description: string,
+    price: number,
+    Icon: any,
+    id: string
+}
+
+interface ServiceCardProps {
+    service: ServiceType
+}
+
+const ServiceCard: FC<ServiceCardProps> = (props) => {
+    const { service: { description, Icon, id, price, title } } = props
+    return (
+        <div className='border border-[#EEEEEE] py-[42px] px-[35px] space-y-[30px]' key={id}>
+            <div className='flex justify-between'>
+                <Icon />
+                <div>
+                    <span className='font-medium text-2xl'>
+                        {price}
+                    </span>
+                    <span className='text-xs inline-block -translate-y-1/2 font-medium'>$</span>
+                </div>
+            </div>
+
+            <h3 className='font-gotu text-[34px] leading-[34px] tracking-tighter w-1/2'>{title}</h3>
+            <p>{description}</p>
+
+        </div>
+    )
+}
+
+
+
+const ServicesData: ServiceType[] = [
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+    {
+        title: 'Pasha Hammam',
+        description: 'From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.',
+        Icon: CaldleAndFlower,
+        price: 38,
+        id: '1'
+    },
+]
+
 
 export default OurServices
