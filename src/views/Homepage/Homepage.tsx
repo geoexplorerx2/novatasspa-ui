615fc5ab9563e6ba78f610ed3b80bef90d765157
MohammedJR <mohammed.j@hotelistan.com>
2023-03-27 11:03:38 +0300

    feat: the Trusted companies component

--- src/views/Homepage/Homepage.tsx
+++ src/views/Homepage/Homepage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroCarousel from '../../components/HeroCarousel/HeroCarousel';
 import HistorySection from '../../components/HistorySection/HistorySection';
+import TrustedCompaniesSection from '../../components/TrustedCompanies/TrustedCompaniesSection';
 import ExperienceSection from './ExperienceSection';
 import OurServices from './OurServices';
 
@@ -11,6 +12,7 @@ const Homepage = () => {
       <ExperienceSection />
       <OurServices />
       <HistorySection />
+      <TrustedCompaniesSection />
     </div>
   )
 };
