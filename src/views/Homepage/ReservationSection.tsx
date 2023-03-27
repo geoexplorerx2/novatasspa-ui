f3a85554bbb8f98152020b8e9a462bc07e4593ba
MohammedJR <mohammed.j@hotelistan.com>
2023-03-27 17:17:07 +0300

    feat: TASSPA-19

--- src/views/Homepage/ReservationSection.tsx
+++ src/views/Homepage/ReservationSection.tsx
@@ -6,15 +6,15 @@ import ReservationForm from '../../components/ReservationForm/ReservationForm'
 
 const ReservationSection = () => {
   return (
-    <div className='relative top-32 h-[970px]'>
-      <div className='absolute z-[10] border-4 border-pink-500 w-full h-full flex flex-col justify-center items-center'>
+    <div className='relative top-4 h-[970px] overflow-hidden'>
+      <div className='absolute z-[10] w-full h-full flex flex-col justify-center items-center'>
         <h2 className='text-white font-gotu text-[64px] tracking-tighter leading-[74px] mb-7'>Make a Quick <br /> Reservation</h2>
         <ReservationForm />
       </div>
      
         <SectionBackground backgroundDivClassNames='h-full'>
          
-          <img src={Chick} className="w-1/2 max-h-full -mt-[8px]"/>
+          <img src={Chick} className="relative top-4 w-1/2 max-h-full -mt-[8px]"/>
         </SectionBackground>
 
       
