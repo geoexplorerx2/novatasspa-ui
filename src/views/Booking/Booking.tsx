2ba74950f2835d0d2eedaa9bf802eb109440fcf4
Laurent Kouassi <laurent.kouassi@hotelistan.com>
2023-04-04 13:57:14 +0300

    booking

---
+++ src/views/Booking/Booking.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Booking = () => {
+  return (
+    <div className='container h-screen'>
+        
+    </div>
+  )
+};
+
+export default Booking;
--- src/views/index.ts
+++ src/views/index.ts
@@ -2,3 +2,4 @@ export { Homepage } from './Homepage';
 export { Services } from './Services';
 export { About } from './About';
 export { Contact } from './Contact';
+export { Booking } from './Booking';
