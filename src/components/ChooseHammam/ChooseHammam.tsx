91a0d9310f47e9b24a3d1dc6112111045a7ccc05
MohammedJR <mohammed.j@hotelistan.com>
2023-04-11 11:55:11 +0300

    fix: the clickable area of the chooseHammam component

--- src/components/ChooseHammam/ChooseHammam.tsx
+++ src/components/ChooseHammam/ChooseHammam.tsx
@@ -31,8 +31,8 @@ const ChooseHamam = () => {
             const { label, value, id } = hammam
             const isActive = selectedItems[value] == true
             return (
-              <div className={`border-2 rounded-[10px] px-5 py-4 flex justify-between items-center ${isActive ? 'border-[#B2A285]' : ''}`} key={id} >
-                <label htmlFor={id} className='text-sm font-medium'>{ label }</label>
+              <div className={`relative border-2 rounded-[10px] px-5 py-4 flex justify-end items-center ${isActive ? 'border-[#B2A285]' : ''}`} key={id} >
+                <label htmlFor={id} className='absolute pl-6 pr-9 w-full h-full top-0 left-0 flex items-center text-sm font-medium'>{ label }</label>
                 <input id={id} name={hammam.value} type='checkbox' className='appearance-none focus:ring-0 focus:ring-transparent rounded-full border-2 border-[#CCCCCC] w-[20px] h-[20px] p-1 text-[#B2A285]' onChange={handleChange}/>
               </div>
             )
