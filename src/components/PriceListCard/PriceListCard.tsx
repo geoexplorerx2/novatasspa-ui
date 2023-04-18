d6d6c43f32069abb7057eea39b7d6d0653641523
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2023-04-18 18:56:31 +0300

    TASPA-62

--- src/components/PriceListCard/PriceListCard.tsx
+++ src/components/PriceListCard/PriceListCard.tsx
@@ -15,16 +15,16 @@ const PriceListCard: FC<PriceListCard_Props> = (props) => {
             <img src={service.image} alt='' className="" />
             
             <div className='ml-[30px] w-[300px]'>
-                <h5 className='text-[24px] leading-[24px]'>{service.name}</h5>
-                <p className='text-[#666B45] text-[16px] font-semibold mt-[5px] font-poppins'>{service.time}</p>
+                <h5 className='md:text-[24px] leading-[24px] text-[20px] '>{service.name}</h5>
+                <p className='text-[#666B45] md:text-[16px] text-[15px] font-semibold mt-[5px] font-poppins'>{service.time}</p>
             </div>
 
             <div className='flex flex-col justify-end '>
                 <h1 className='text-[#423930] text-[34px] font-semibold pl-[54px] font-poppins relative'>
-                    <span>{service.price}</span>
-                    <span className='absolute top-0 text-[24px]'>€</span>
+                    <span className='md:text-[34px] text-[24px]'>{service.price}</span>
+                    <span className='absolute top-0 md:text-[34px] text-[18px]'>€</span>
                 </h1>
-                <button className='text-[13px] font-medium text-[#423930] bookButton inline-flex'>
+                <button className='text-[13px] font-medium text-[#423930] bookButton inline-flex w-[200px]'>
                     Book Now 
                     <img src={ArrowRight} alt='' className='w-[16px] ml-[13px] translate-y-[1px]' />
                 </button>
