9be9c88610480489aa57c7fd3e1b1ac58039d08c
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-25 06:31:43 -0700

    translate

--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -5,7 +5,8 @@ import Button from '../../lib/Button/Button';
 import ButtonWithIcon from '../../lib/Button/ButtonWithIcon';
 import languageIcon from "../../assets/icons/languageIcon.svg";
 import { Languages } from '../constants/Languages';
-
+import { pathname } from '../../data/pathname';
+import { routename } from '../../data/pathname';
 interface LanguagePickerProps {
     bgColor?: string;
     openDropdownMenu: any,
@@ -87,26 +88,34 @@ const LanguagePicker: FC<LanguagePickerProps> = ({ bgColor, openDropdownMenu, se
                             static
                             className={`sub-menu will-change-transform transform w-[260px] right-7 absolute ${classAsProp}`}
                         >
-                            {({close}) => (
-                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10 text-sm bg-white dark:bg-neutral-900 py-2 grid grid-cols-2 gap-2">
+                            {({ close }) => (
+                                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-white dark:ring-opacity-10 text-sm bg-white dark:bg-neutral-900 py-2 grid grid-cols-2 gap-2">
 
-                                {
-                                    Languages?.map((lang: any) => (
-                                        <div className="flex justify-arround mx-2 ">
-                                            <Button
-                                                rounded=""
-                                                className={`${activeLang == lang.id ? 'bg-[#3944b3] text-[white] ' : 'bg-[#fff] text-[#000]'} rounded-[8px] hover:border-[#423930]  border-2 border-[rgba(57, 68, 179, 20%)]  flex justify-arround w-[106px] h-[50px] dark:text-white`}
-                                                onClick={() => {
-                                                    handleLan(lang)
-                                                    close()
-                                                }}
-                                            >
-                                                {lang.name}
-                                            </Button>
-                                        </div>
-                                    ))
-                                }
-                            </div>
+                                    {
+                                        Languages?.map((lang: any) => (
+                                            <div className="flex justify-arround mx-2 ">
+                                                <Button
+                                                    rounded=""
+                                                    className={`${activeLang == lang.id ? 'bg-[#3944b3] text-[white] ' : 'bg-[#fff] text-[#000]'} rounded-[8px] hover:border-[#423930]  border-2 border-[rgba(57, 68, 179, 20%)]  flex justify-arround w-[106px] h-[50px] dark:text-white`}
+                                                    onClick={() => {
+                                                        handleLan(lang)
+                                                        close()
+                                                        if (routename.length == 4) {
+                                                            console.log('routename is :', `${routename[0]}/${routename[1]}/${routename[2]}/${routename[3]}`)
+                                                        } else {
+                                                            console.log('routename is :', `${routename[0]}/${routename[1]}/${routename[2]}/${lang.id}/${routename[4]}`)
+                                                            window.location.href = `${routename[0]}/${routename[1]}/${routename[2]}/${lang.id}/${routename[4]}`
+                                                        }
+
+
+                                                    }}
+                                                >
+                                                    {lang.name}
+                                                </Button>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
                             )}
                         </Popover.Panel>
                     </Transition>
